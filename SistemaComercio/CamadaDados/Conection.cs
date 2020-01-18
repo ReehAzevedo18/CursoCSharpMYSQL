8ab8c4bfefe12dc01f0ecb59c53b60ddec7c5f99
ReehAzevedo18 <58194615+ReehAzevedo18@users.noreply.github.com>
2020-01-18 12:28:59 -0300

    Inclusão da conexão com o BD e dados da tabela Categorias

---
+++ SistemaComercio/CamadaDados/Conection.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CamadaDados
+{
+    //Classe de conexão com o banco de dados
+    class Conection
+    {
+        //Falta preencher o nome do BD e o nome do banco criado
+        public static String Cn = "Data Source = ; Initial Catalog = dbcomercio; Integrated Security=true";
+    }
+}
